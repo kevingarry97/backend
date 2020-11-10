3410277ed12b7e6214408918cc70d736ac9fba6b
kevingarry97 <kevingarry97@gmail.com>
2020-11-10 02:49:09 -0800

    Middlewares

--- index.js
+++ index.js
@@ -49,22 +49,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    const error = new Error("Not found");
-    error.status = 404;
-    next(error);
-});
-  
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-      error: {
-        message: error.message,
-      },
-    });
-    next();
-});
-
 app.get('/api', (req, res) => {
     res.send("Hello world");
 })
